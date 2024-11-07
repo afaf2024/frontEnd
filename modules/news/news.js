415b9a78abaeab58fd4317543e9cf0c5b6fa2cf3
felix-caba <felixcaba@proton.me>
2024-11-07 18:41:56 +0100

    Refactor news fetching to use API endpoint and streamline data handling. Added CACHE at the BACKEND, so proxy cors faster to load

--- modules/news/news.js
+++ modules/news/news.js
@@ -5,83 +5,53 @@ const cardTitles = document.querySelectorAll('.ag-courses-item_title');
 const cardDate = document.querySelectorAll('.ag-courses-item_date');
 const cardDescription = document.querySelectorAll('.ag-courses-item_description');
 
-const xmlUrlSpain = "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/internacional/portada";
-const xmlUrlGerman = "https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml";
-const xmlUrlItalian = "https://www.ansa.it/sito/notizie/mondo/mondo_rss.xml";
-const xmlUrlEnglish = "https://www.reutersagency.com/feed/?taxonomy=best-sectors&post_type=best";
+
+
 
 
 async function setNews() {
 
     const lang = localStorage.getItem("lang");
-
+    
     const data = await getNews(lang);
-
-    console.log(data);
 
     let cont = -1;
 
     cardTitles.forEach(cardTitle => {
-
+        
         cardTitle.innerHTML = data[++cont].title;
         cardDate[cont].innerHTML = data[cont].pubDate;
         cardDescription[cont].innerHTML = data[cont].description;
 
-
+        
         // Open a new tab but dont focus on it (open in background)
         const card = cardTitle.parentElement;
 
         card.addEventListener('click', () => {
             window.open(data[cont].link, '_blank');
         });
+        
 
-
-
-
+        
+        
     });
-
+    
 }
 
 
-async function getNews(lang){
+async function getNews(lang) {
 
-    let xmlUrl = "";
-
-    switch(lang){
-        case "es":
-            xmlUrl = xmlUrlSpain;
-            break;
-        case "de":
-            xmlUrl = xmlUrlGerman;
-            break;
-        case "it":
-            xmlUrl = xmlUrlItalian;
-            break;
-        case "en":
-            xmlUrl = xmlUrlEnglish;
-            break;
-        default:
-            xmlUrl = xmlUrlEnglish;
-            break;
-    }
-
-    const response = await fetch(xmlUrl);
-    const xml = await response.text();
-    const parser = new DOMParser();
-    const xmlDOM = parser.parseFromString(xml, 'text/xml');
-    // convert to json
-    const items = xmlDOM.querySelectorAll('item');
-    const data = [];
-
-    items.forEach(item => {
-        data.push({
-            title: item.querySelector('title').textContent,
-            description: item.querySelector('description').textContent,
-            link: item.querySelector('link').textContent,
-            pubDate: item.querySelector('pubDate').textContent
-        });
+    const response = await fetch(`https://erasmus.ieszaidinvergeles.es/fakeNews/public/api/news/${lang}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
     });
 
+    // json
+    const data = await response.json();
+    console.log(data);
     return data;
     
 }
